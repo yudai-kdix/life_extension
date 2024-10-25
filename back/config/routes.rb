d70f5ead1da86da83023aa180ffeaa244cccfc5b
yudai-kdix <yudai.homma222@gmail.com>
2024-10-25 14:27:52 +0900

    ユーザーコントローラーにCRUDアクションを追加し、ルーティングに行動ログ取得用のエンドポイントを追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :characters, only: [:index, :show, :create, :update, :destroy]
   resources :action_logs, only: [:index, :show, :create]
 
-
+  # 行動ログ取得用のエンドポイント
+  get '/users/:id/action', to: 'action_logs#user_actions'
 end
