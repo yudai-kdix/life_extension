a49a7de77e0dde4063e34192fad50f8d10aafece
hamuhamu5hamu <fujisyo5090@au.com>
2024-11-10 20:29:57 +0900

    add home仮完成

--- front/src/components/ActionButton.tsx
+++ front/src/components/ActionButton.tsx
@@ -7,13 +7,16 @@ interface ActionButtonProps {
 
 export function ActionButton({ action, onClick }: ActionButtonProps) {
   return (
-    <li className="flex-shrink-0 list-none">
+    // <li className="flex-shrink-0 list-none">
+    <div className="">
       <button
         onClick={() => onClick(action)}
-        className="relative w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32 rounded-full bg-white text-white flex items-center justify-center transition-all duration-300 ease-in-out hover:scale-110"
+        className="relative w-24 h-24 rounded-full bg-white text-white flex items-center justify-center transition-all duration-300 ease-in-out hover:scale-110"
       >
-        <span className="text-3xl md:text-5xl lg:text-7xl">{action.icon}</span>
+        <span className="text-5xl">{action.icon}</span>
       </button>
-    </li>
+    </div>
+
+    // </li>
   );
 }
