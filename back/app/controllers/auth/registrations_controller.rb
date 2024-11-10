33ccfd24bdff61adc995412274c68f9bb011ce76
yudai-kdix <yudai.homma222@gmail.com>
2024-11-10 20:09:16 +0900

    格闘中

--- back/app/controllers/auth/registrations_controller.rb
+++ back/app/controllers/auth/registrations_controller.rb
@@ -14,8 +14,8 @@ class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
     return unless @resource.persisted?
 
     token = @resource.create_new_auth_token
-    response.set_header('access-token', token['access-token'])
-    response.set_header('client', token['client'])
-    
+    # response.set_header('access-token', token['access-token'])
+    # response.set_header('client', token['client'])
+    render json: { user: @resource, token: token }, status: :created
   end
 end
